a7ecdc8b0eb24be967ac8b951bd254608865bfd9
Kreyl <laystoll@yandex.ru>
2016-09-10 16:07:53 +0300

    + Non-linear volume adjustment RC

--- Steto2_fw/main.cpp
+++ Steto2_fw/main.cpp
@@ -18,46 +18,69 @@ App_t App;
 static TmrKL_t TmrPwrPoll {MS2ST(99), EVT_PWR_POLL, tktPeriodic};
 static const PinOutput_t PinPwr {PWR_PIN};
 
-// Digital Potentiometer
-#define PinDelay()  {__NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();}
-
+// ==== Digital Potentiometer ====
 class DigPot_t {
 private:
+    int32_t Value = 0;
     const PinOutput_t UD {DPOT_UD_PIN};
     const PinOutput_t CS {DPOT_CS_PIN};
 public:
-    void Init() const {
+    void Increase() {
+        if     (Value < 10) MoveUp(1);
+        else if(Value < 20) MoveUp(2);
+        else if(Value < 30) MoveUp(3);
+        else if(Value < 40) MoveUp(4);
+        else if(Value < 50) MoveUp(5);
+        else if(Value < 60) MoveUp(6);
+    }
+    void Decrease() {
+        if(Value == 1) return;
+        if     (Value < 10) MoveDown(1);
+        else if(Value < 20) MoveDown(2);
+        else if(Value < 30) MoveDown(3);
+        else if(Value < 40) MoveDown(4);
+        else if(Value < 50) MoveDown(5);
+        else if(Value < 60) MoveDown(6);
+    }
+
+    void MoveDown(int32_t Cnt) {
+        Value -= Cnt;
+        if(Value < 0) Value = 0;
+        chSysLock();
+        UD.Lo();
+        CS.Lo();
+        while(Cnt != 0) {
+            UD.Lo();
+            UD.Hi();
+            Cnt--;
+        }
+        CS.Hi();
+        chSysUnlock();
+    }
+    void MoveUp(int32_t Cnt) {
+        Value += Cnt;
+        if(Value > 64) Value = 64;
+        chSysLock();
+        UD.Hi();
+        CS.Lo();
+        while(Cnt != 0) {
+            UD.Lo();
+            UD.Hi();
+            Cnt--;
+        }
+        CS.Hi();
+        chSysUnlock();
+    }
+    void Init() {
         UD.Init();
         CS.Init();
         CS.Hi();
-    }
-    void MoveDown(uint32_t Cnt) const {
-        chSysLock();
-        UD.Lo(); PinDelay();
-        CS.Lo(); PinDelay();
-        while(Cnt != 0) {
-            UD.Lo(); PinDelay();
-            UD.Hi(); PinDelay();
-            Cnt--;
-        }
-        CS.Hi();
-        chSysUnlock();
-    }
-    void MoveUp(uint32_t Cnt) const {
-        chSysLock();
-        UD.Hi(); PinDelay();
-        CS.Lo(); PinDelay();
-        while(Cnt != 0) {
-            UD.Lo(); PinDelay();
-            UD.Hi(); PinDelay();
-            Cnt--;
-        }
-        CS.Hi();
-        chSysUnlock();
+        MoveDown(72);
+        MoveUp(1);
     }
 };
 
-const DigPot_t DigPot;
+DigPot_t DigPot;
 
 int main(void) {
     // ==== Init Vcore & clock system ====
@@ -110,12 +133,12 @@ void App_t::ITask() {
             while(BtnGetEvt(&EInfo) == OK) {
                 if(EInfo.Type == bePress or EInfo.Type == beRepeat) {
                     if(EInfo.BtnID == btnUp) {
-                        Uart.Printf("Up\r");
-                        DigPot.MoveUp(8);
+//                        Uart.Printf("Up\r");
+                        DigPot.Increase();
                     }
                     else if(EInfo.BtnID == btnDown) {
-                        Uart.Printf("Down\r");
-                        DigPot.MoveDown(8);
+//                        Uart.Printf("Down\r");
+                        DigPot.Decrease();
                     }
                 }
             }
